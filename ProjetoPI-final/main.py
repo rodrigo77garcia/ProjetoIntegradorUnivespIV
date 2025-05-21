aa82045405c1d93448cb6e34841cc4a0fd459b00
rodrigo77garcia <matcomp.rodrigo@gmail.com>
2025-05-21 17:30:43 -0300

    update-variaveis

--- ProjetoPI-final/main.py
+++ ProjetoPI-final/main.py
@@ -1,17 +1,29 @@
 from app import create_app
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv
 
 
 
 app = create_app()
 
-# Configuração do banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql -h gondola.proxy.rlwy.net -u root -p AyqLoutFQEZApbsRHappGyihdqOEOoRb --port 26546 --protocol=TCP railway'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 
 db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.config['DEBUG']=True
     app.run(debug=True)
+
+    db_url = os.getenv('DATABASE_URL')
+    if not db_url:
+        raise ValueError("DATABASE_URL não configurado.")
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db = SQLAlchemy(app) 
