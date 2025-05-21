0daf820d3335778d210262777046d35c035a9905
rodrigo77garcia <matcomp.rodrigo@gmail.com>
2025-05-21 18:07:17 -0300

    novas classes para deploy

--- ProjetoPI-final/app/__init__.py
+++ ProjetoPI-final/app/__init__.py
@@ -1,25 +1,27 @@
 from flask import Flask
-
+from .extensions import db
+from dotenv import load_dotenv
+import os
 
 def create_app():
+    # Carregar variáveis do .env
+    load_dotenv()
+
     app = Flask(__name__)
-    
 
     # Configurações do banco de dados
-    app.config['MYSQL_HOST']     = 'localhost'
-    app.config['MYSQL_USER']     = 'root'
-    app.config['MYSQL_PASSWORD'] = 'Elisangela@77'
-    app.config['MYSQL_DB']       = 'gestao_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    # Inicializa a extensão SQLAlchemy
+    db.init_app(app)
+
+    # Registro dos blueprints
     with app.app_context():
         from .routes import bp as routes_bp
         app.register_blueprint(routes_bp, url_prefix='/')
 
         from .chat import bp_chat
         app.register_blueprint(bp_chat)
-
-        from .db import init_db, close_db
-        init_db()
-        app.teardown_appcontext(close_db)
 
     return app
--- ProjetoPI-final/main.py
+++ ProjetoPI-final/main.py
@@ -1,29 +1,7 @@
 from app import create_app
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
-
-load_dotenv
-
-
 
 app = create_app()
 
-
-
-
-db = SQLAlchemy(app)
-
 if __name__ == '__main__':
-    app.config['DEBUG']=True
+    app.config['DEBUG'] = True
     app.run(debug=True)
-
-    db_url = os.getenv('DATABASE_URL')
-    if not db_url:
-        raise ValueError("DATABASE_URL não configurado.")
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db = SQLAlchemy(app) 
