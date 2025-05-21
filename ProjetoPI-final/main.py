e8c2551f5df5ee6146124135178157911a991076
rodrigo77garcia <matcomp.rodrigo@gmail.com>
2025-05-21 16:42:04 -0300

    deploy-prd-teste

--- ProjetoPI-final/main.py
+++ ProjetoPI-final/main.py
@@ -1,6 +1,16 @@
 from app import create_app
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = create_app()
+
+# Configuração do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql -h gondola.proxy.rlwy.net -u root -p AyqLoutFQEZApbsRHappGyihdqOEOoRb --port 26546 --protocol=TCP railway'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.config['DEBUG']=True
